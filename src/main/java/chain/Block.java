7035175c1292f666d075f45e24dd3fb848292418
Tai Bui Tan <venkug@sg.uob>
2018-12-17 16:49:43 +0800

    initial code

---
+++ src/main/java/chain/Block.java
@@ -0,0 +1,43 @@
+package chain;
+import java.util.Date;
+
+public class Block {
+	
+	public String hash;
+	public String previousHash; 
+	private DataTransaction transactionData; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+	private int nonce;
+	
+	//Block Constructor.  
+	public Block(DataTransaction data,String previousHash ) {
+		this.transactionData = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+	}
+	
+	//Calculate new hash based on blocks contents
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				transactionData.data 
+				);
+		return calculatedhash;
+	}
+	
+	//Increases nonce value until hash target is reached.
+	public void mineBlock(int difficulty) {
+		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+	
+}
+
---
+++ src/main/java/chain/DataTransaction.java
@@ -0,0 +1,11 @@
+package chain;
+
+
+public class DataTransaction {
+	String data;
+
+	public DataTransaction(String data) {
+		this.data = data;
+	}
+
+}
